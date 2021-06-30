b327c1572bdc0cff4b9df6d5d3d83f7c440e88f3
bakuchi- <helveticaf0015se@gmail.com>
2021-06-30 19:41:26 +0900

    add mock

---
+++ contracts/mock/CoinMock.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "../interfaces/IOracleRelayer.sol";
+
+contract CoinMock is ERC20 {
+    constructor() ERC20("Rai Reflex Index", "RAI") {}
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
+    }
+}
---
+++ contracts/mock/OracleRelayerMock.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "../interfaces/IOracleRelayer.sol";
+
+contract OracleRelayerMock is IOracleRelayer {
+    uint256 private _redemptionPrice;
+    uint256 public override redemptionPriceUpdateTime;
+
+    function contractEnabled() public pure override returns (uint256) {
+        return 1;
+    }
+
+    function redemptionPrice() public override returns (uint256) {
+        return _redemptionPrice;
+    }
+
+    function setRedemptionPrice(uint256 redemptionPrice_) public returns (uint256) {
+        _redemptionPrice = redemptionPrice_;
+        return _redemptionPrice;
+    }
+}
